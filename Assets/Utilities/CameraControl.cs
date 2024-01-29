974d08beae6a9f53efd22395521ec06bb921e879
FLOYD993 <142295304+FLOYD993@users.noreply.github.com>
2024-01-29 11:03:51 +0800

    视差效果
    脚本camera control添加（待调整）

--- Assets/Utilities/CameraControl.cs
+++ Assets/Utilities/CameraControl.cs
@@ -9,6 +9,12 @@ public class CameraControl : MonoBehaviour
     private CinemachineConfiner2D confiner;
     public CinemachineImpulseSource impulseSource;
     public VoidEventSO cameraShakeEvent;
+
+    //位移视差
+    public Transform target; //玩家位置
+    public Transform farBackground, midBackground; //远景和中景的位置
+    private Vector2 lastPos; //最后一次相机的位置
+
     private void Awake()
     {
         confiner=GetComponent<CinemachineConfiner2D>();
@@ -16,6 +22,18 @@ public class CameraControl : MonoBehaviour
     private void Start()
     {
         GetNewCameraBounds();
+        lastPos = transform.position; //记录相机的初始位置
+    }
+    private void Update()
+    {
+        //计算相机在上一帧和当前帧之间的移动距离
+        Vector2 amountToMove = new Vector2(transform.position.x - lastPos.x, transform.position.y - lastPos.y);
+
+        //根据相机移动的距离，移动远景和中景的位置
+        farBackground.position += new Vector3(amountToMove.x, amountToMove.y, 0f);
+        midBackground.position += new Vector3(amountToMove.x * 0.5f, amountToMove.y * 0.5f, 0f);
+
+        lastPos = transform.position;
     }
     private void OnEnable()
     {
